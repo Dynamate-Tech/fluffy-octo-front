fce0dd3fc6792274351cb299b88b18171ee4800b
Dynamate-Tech <dynamate.promo@gmail.com>
2025-07-03 16:10:29 +0800

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminDashboard from './pages/admin'; // make sure this path is correct
+import BatchesPage from './pages/BatchesPage';
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<AdminDashboard />} />
         <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/batches" element={<BatchesPage />} />
       </Routes>
     </Router>
   );
