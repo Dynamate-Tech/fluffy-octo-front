b3717a5f38522b7bdbe323328b2ebd3f02a1d8fa
Dynamate-Tech <dynamate.promo@gmail.com>
2025-07-03 16:24:55 +0800

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminDashboard from './pages/admin'; // make sure this path is correct
-import BatchesPage from './pages/BatchesPage';
 
 const App = () => {
   return (
